aa87d6ca545a5b6703a8fb5cd2b2fff1e30e1c00
mbadreau <matthieu.badreau@gmail.com>
2021-01-07 12:25:56 +0100

    import vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '@fortawesome/fontawesome-free/js/all.js';
 import App from './App.vue'
 
+Vue.use(VueRouter)
 Vue.use(Buefy, {
     defaultIconPack: 'fas',
     defaultContainerElement: '#content',
